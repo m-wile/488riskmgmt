93fc09f3ac4f58c637a92373daa5ac93f7cef7ae
Olivier Haley <129203990+tigerwoodsjr@users.noreply.github.com>
2024-03-05 15:12:02 -0700

    nice layout2

--- server.R
+++ server.R
@@ -1,18 +1,17 @@
 library(shiny)
-library(plotly)
+library(DT)
 library(tidyverse)
 library(RTL)
 
 server <- function(input, output, session) {
   
   bonds_df <- reactiveValues(data = data.frame(
-    coupon = numeric(),
-    ytm = numeric(),
-    par_value = numeric(),
-    ttm = numeric(),
-    principal = numeric(),
-    price = numeric()
-    
+    C = numeric(),
+    YTM = numeric(),
+    Par = numeric(),
+    TTM = numeric(),
+    "Bonds held" = numeric(),
+    Price = numeric()
   ))
   
   #######################################################
@@ -27,11 +26,11 @@ server <- function(input, output, session) {
   #######################################################
   observeEvent(input$addBond, {
     showModal(modalDialog(
-      textInput("coupon", "Coupon Rate:", "4"),
-      textInput("ytm", "Yield to Maturity:", "5"),
+      textInput("coupon", "Annual Coupon Rate (%):", "4"),
+      textInput("ytm", "Yield to Maturity (%):", "5"),
       textInput("par_value", "Par Value:", "100"),
-      textInput("ttm", "Time to Maturity:", "10"),
-      textInput("principal", "Principal:", "1000"),
+      textInput("ttm", "Years to Maturity:", "10"),
+      textInput("principal", "Principal ($):", "1000"),
       actionButton("saveBond", "Save Bond"),
       easyClose = TRUE)
     )
@@ -40,23 +39,66 @@ server <- function(input, output, session) {
   #######################################################
   # Save Bond when Save Bond Button is clicked
   #######################################################
-
-observeEvent(input$saveBond, {
-  new_bond <- data.frame(
-    coupon = as.numeric(input$coupon),
-    ytm = as.numeric(input$ytm),
-    par_value = as.numeric(input$par_value),
-    ttm = as.numeric(input$ttm),
-    principal = as.numeric(input$principal),
-    price = as.numeric(RTL::bond(C = as.numeric(input$coupon), ytm = as.numeric(input$ytm), T2M = as.numeric(input$ttm)))
-  )
-  removeModal()
-  bonds_df$data <- rbind(bonds_df$data, new_bond)
-})
-
-  #######################################################
-  # Output df
-  #######################################################
-  output$bondsss <- renderTable(bonds_df$data)
+  observeEvent(input$saveBond, {
+    new_bond <- data.frame(
+      C = as.numeric(input$coupon),
+      YTM = as.numeric(input$ytm),
+      Par = as.numeric(input$par_value),
+      TTM = as.numeric(input$ttm),
+      "Bonds held" = as.numeric(input$principal),
+      Price = round(as.numeric(bond_cpp_call(ytm = as.numeric(input$ytm), 
+                                             C = as.numeric(input$coupon), 
+                                             T2M = as.numeric(input$ttm), 
+                                             m = 2, output = "price")), 1)
+    )
+    removeModal()
+    bonds_df$data <- rbind(bonds_df$data, new_bond)
+  })
   
+  #######################################################
+  # Output df with checkboxes
+  #######################################################
+  output$bond_table <- renderDT({
+    datatable(bonds_df$data, 
+              options = list(
+                pageLength = 10,
+                lengthMenu = c(5, 10, 15),
+                dom = 'lfrtip',
+                initComplete = JS(
+                  "function(settings, json) {",
+                  "$(this.api().table().header()).css({'background-color': '#428bca', 'color': '#fff'});",
+                  "}"),
+                preDrawCallback = JS(
+                  "function() {",
+                  "$(this.api().table().header()).css({'background-color': '', 'color': ''});",
+                  "}"),
+                drawCallback = JS(
+                  "function() {",
+                  "$(this.api().table().header()).css({'background-color': '#428bca', 'color': '#fff'});",
+                  "}"),
+                language = list(
+                  search = '<span>Search:</span> _INPUT_',
+                  searchPlaceholder = 'Search...'
+                )
+              ),
+              selection = list(mode = 'multiple', target = 'row'),
+              editable = TRUE
+    )
+  })
+  
+  #######################################################
+  # Delete selected rows
+  #######################################################
+  
+  observeEvent(input$deleteButton, {
+    selected_rows <- input$bond_table_rows_selected
+    bonds_df$data <- bonds_df$data[-selected_rows, , drop = FALSE]
+  })
+  
+  #######################################################
+  # Add Delete Button
+  #######################################################
+  output$deleteButton <- renderUI({
+    actionButton("deleteButton", "Delete Selected Bonds")
+  })
 }
--- ui.R
+++ ui.R
@@ -9,22 +9,19 @@ ui <- fluidPage(
   sidebarLayout(
     
     sidebarPanel(
-      uiOutput("addBondButton")),
-    
-    mainPanel(
-      h3("Price of the bond:"),
-      
-
-      h3("Sensitivity of the price by yield:"),
-      tableOutput("bondsss"),
-
+      uiOutput("addBondButton"),
+      DTOutput("bond_table"),
+      uiOutput("deleteButton"),
       h6("Instruction:"),
       h6("You should insert the maturity, coupon, yield and redeption price"),
       h6("a) Select the maturity in years"),
       h6("b) Insert coupon and yield as percentage"),
       h6("c) Insert redemption price as percentage "),
       h6("You will get the quotation of the bond as percentage and a graphic"),
-      h6("of the sensibility of the price by yield")
+      h6("of the sensibility of the price by yield")),
+    
+    mainPanel(
+      
     )
     
     
