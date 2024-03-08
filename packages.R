fb34270ee4967b3b1ff704e114a27eb2eff9e143
rasharm1 <raina.sharma1@ucalgary.ca>
2024-03-07 23:42:13 -0700

    modify layout

--- packages.R
+++ packages.R
@@ -1,4 +1,4 @@
-packages <- c("bslib", "DT", "htmlwidgets", "plotly", "Rcpp", "RTL", "tidyquant", "tidyverse", "TTR", "zoo")
+packages <- c("bslib", "DT", "htmlwidgets", "plotly", "Rcpp", "RTL", "stats", "shinyWidgets", "tidyquant", "tidyverse", "TTR")
 new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
 if (length(new.packages)){
   install.packages(new.packages, dependencies = TRUE)
--- ui.R
+++ ui.R
@@ -3,20 +3,35 @@ library(plotly)
 library(shiny)
 
 ui <- shiny::fluidPage(
-  shiny::titlePanel("Bond Price"),
+  shiny::titlePanel(title = "Bond Prices"),
+    shiny::fluidRow(
+      shiny::actionButton("help", "Instructions"), style = "float:right;"), 
+  theme = bslib::bs_theme(bootswatch = "flatly"),
+  shiny::br(),
   shiny::sidebarLayout(
     shiny::sidebarPanel(
-      width = 2,
+      align = "center",
+      width = 2, 
+      shiny::h5("Add Bonds"), 
+      shiny::HTML("<h6 style='text-align:center;'> 
+                  <em> Click instructions for use </em> 
+                  </h6>"),
+      shiny::br(),
       shiny::uiOutput("addBondButton"),
+      shiny::br(), 
       shiny::uiOutput("deleteButton"),
-      shiny::actionButton("loadRandomPortfolio", "Load Random Portfolio"),
-      shiny::h6("Instruction:"),
-      shiny::h6("You should insert the maturity, coupon, yield and redeption price"),
-      shiny::h6("a) Select the maturity in years"),
-      shiny::h6("b) Insert coupon and yield as percentage"),
-      shiny::h6("c) Insert redemption price as percentage "),
-      shiny::h6("You will get the quotation of the bond as percentage and a graphic"),
-      shiny::h6("of the sensibility of the price by yield")),
+      shiny::br(),
+      shiny::actionButton("loadRandomPortfolio", "Load Random Portfolio")),
+      # shiny::uiOutput("addBondButton"),
+      # shiny::uiOutput("deleteButton")),
+      # shiny::actionButton("loadRandomPortfolio", "Load Random Portfolio"),
+      # shiny::h6("Instruction:"),
+      # shiny::h6("You should insert the maturity, coupon, yield and redeption price"),
+      # shiny::h6("a) Select the maturity in years"),
+      # shiny::h6("b) Insert coupon and yield as percentage"),
+      # shiny::h6("c) Insert redemption price as percentage "),
+      # shiny::h6("You will get the quotation of the bond as percentage and a graphic"),
+      # shiny::h6("of the sensibility of the price by yield")),
     shiny::mainPanel(
       width = 10, 
       # plotly::plotlyOutput("ytm_price_plot"), 
